0e81733bd412515670e23453e0861fefa29880e0
Pankaj Kumar <panxpanwar@gmail.com>
2022-08-04 18:07:36 +0530

    Updated Report in test Runner Class

--- src/test/java/TestRunner/TestRunner.java
+++ src/test/java/TestRunner/TestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.junit.*;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/FeatureFile",
-glue = "Steps" )
+glue = "Steps", 
+plugin = {"pretty","html:target/cucumber-reports.html"}, 
+monochrome = true)
 
 public class TestRunner {
 
